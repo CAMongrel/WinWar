7332310d74b1d63a11cb4ae8b4d656cec5f1b06b
Henning Thoele <hthoele@gmail.com>
2013-02-04 17:07:26 +0100

    Minor formatting change

--- WinWarRT/MainGame.cs
+++ WinWarRT/MainGame.cs
@@ -1,6 +1,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
+using System.Text;
 using WinWarRT.GameScreens;
 
 namespace WinWarRT
@@ -123,7 +124,8 @@ namespace WinWarRT
                 return;
             }
 
-            MovieGameScreen.PlayMovie("TITLE.WAR", delegate
+            MovieGameScreen.PlayMovie("TITLE.WAR", 
+                delegate
                 {
                     SetNextGameScreen(new MenuGameScreen());
                 });
