d46ad933ab3ea4dd8890f5c0f863b653980169f7
Henning Thoele <hthoele@gmail.com>
2013-02-05 18:35:37 +0100

    Added simple settings pane

---
+++ WinWarRT/GameSettingsFlyout.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Benutzersteuerelement" ist unter http://go.microsoft.com/fwlink/?LinkId=234236 dokumentiert.
+
+namespace WinWarRT
+{
+    public sealed partial class GameSettingsFlyout : UserControl
+    {
+        public GameSettingsFlyout()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
