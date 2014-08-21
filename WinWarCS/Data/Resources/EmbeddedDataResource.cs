a4962d116745c71fddcdd531b14778ba4d5fbc49
Henning Thoele <hthoele@gmail.com>
2014-08-21 12:23:43 +0200

    Added resource management (will be expanded later). Sprite data is now cached for each sprite, instead of always creating new textures for repeating sprites. Added performance measurement class.

---
+++ WinWarCS/Data/Resources/EmbeddedDataResource.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace WinWarCS.Data.Resources
+{
+   internal class EmbeddedDataResource : BasicResource
+   {
+      internal EmbeddedDataResource(WarResource data)
+      {
+         Init(data);
+      }
+
+      private void Init(WarResource data)
+      {
+
+      }
+   }
+}
+
