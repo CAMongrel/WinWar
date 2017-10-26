e54f0875ab3076bba21a5d129e906e422316576f
Henning Thoele <hthoele@gmail.com>
2017-10-26 17:53:23 +0200

    Add simple unit test project (not used for production yet)

---
+++ DataWarLib/DataWarLibTests/PerformanceTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+using NUnit.Framework;
+using WinWarCS.Util;
+
+namespace DataWarLibTests
+{
+   [TestFixture ()]
+   public class PerformanceTests
+   {
+      [Test ()]
+      public void TestNotEnabled ()
+      {
+         Performance.Enabled = false;
+
+         Performance.Push ("Test");
+
+         // Wait five milliseconds
+         Thread.Sleep (5);
+
+         double result = Performance.Pop ();
+
+         Assert.IsTrue (result == 0.0);
+      }
+
+      [Test ()]
+      public void TestEnabled ()
+      {
+         Performance.Enabled = true;
+
+         Performance.Push ("Test");
+
+         // Wait five milliseconds
+         Thread.Sleep (5);
+
+         double result = Performance.Pop ();
+
+         // Due to inexact timing, we must use an epsilon test
+         // Should be close though
+         Assert.IsTrue (result >= 0.005 && result <= 0.007);
+      }
+   }
+}
